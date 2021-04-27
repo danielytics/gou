6c0c4da3d95f2de5ad76ff0ccc5ccec6e4e01e51
danielytics <danielytics@users.noreply.github.com>
2021-04-27 02:49:19 +0100

    Make sure the sdk/components directory exists

--- generate_comoponents.sh
+++ generate_comoponents.sh
@@ -4,6 +4,10 @@ if [ ! -f tools/generate-components ]; then
     clang++ -std=c++17 -Ivendor/cxxopts/include -Ivendor/orderedmap/include -Ivendor/toml11 tools/components-generator/*.cpp -o tools/generate-components
 fi
 
+if [ ! -d sdk/components ]; then
+  mkdir sdk/components
+fi
+
 for DIR in ./engine/ `ls -d modules/*/`
 do
     FILE="${DIR}components.toml"
